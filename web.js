d446bf13998a8fab63fea1a689798fb1cdb5c9af
Adriean Khisbe <adriean.khisbe@live.fr>
2013-07-11 20:13:02 +0000

    Add index, that web.js read

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var buffer = new Buffer(256);
+
+fs.readFileSync('./index.html', function (err, data) {
+    if (err) throw err;
+   buffer.write(data,"utf-8");
+});
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
